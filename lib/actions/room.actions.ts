3712db74b82bfd735c80033a7adbd06179e2b8a8
kalerie <valeriiakuryliak@gmail.com>
2024-11-26 11:13:23 +0100

    - permision hotfix

--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -23,7 +23,7 @@ export const createDocument = async ({userId, email}: CreateDocumentParams) => {
         const room = await liveblocks.createRoom(roomId, {
             metadata,
             usersAccesses,
-            defaultAccesses: []
+            defaultAccesses: ["room:write"]
         });
         
         revalidatePath('/');
